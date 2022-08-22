5ab74461613b39ba2759459b15afb2be00a072e5
Mauricio Garello <87575987+maukovenich@users.noreply.github.com>
2022-08-22 01:55:56 -0300

    1

--- desafio_02/script.js
+++ desafio_02/script.js
@@ -1,42 +1,68 @@
-let prestamo = 0,
-    interes = 0,
-    new_prestamo = 0,
-    tres = 0,
-    seis = 0,
-    nueve = 0,
-    ok = false;
+let salir = false,
+    db_prestamo = [],
+    monto = 0;
+
+
+class Prestamo {
+    constructor(monto, cuotas, interes) {
+        this.monto = monto;
+        this.cuotas = cuotas;
+        this.interes = monto * interes / 100;
+    }
+
+    get_datos() {
+        alert(`Info sobre el préstamo solicitado:
+                Monto: $` +this.monto + `
+                Cuotas: ` +this.cuotas + `
+                Total a pagar con interés: ` +this.interes);
+    }
+
+    set_monto(monto) {
+        this.monto = monto;
+    }
+
+    set_interes(int) {
+        this.interes = monto * int / 100;
+    }
+    
+    set_total() {
+        this.total = monto + this.interes;
+    }
+  
+}
+
 
 do {
-function tres_cuotas(prestamo) {
-    prestamo = parseInt(prompt("Ingrese el monto a calcular en pesos argentinos: "), 10);
-    if (Number.isNaN(prestamo)) return alert("Este simulador sólo admite números como parámetro");
-    tres++;
-    for (i = 0; i == 0; i++) {
-        interes = prestamo * 0.15;
-    }
-    alert("Para la cantidad de: $" +prestamo.toFixed(2)+ " , en 3 cuotas, el interés a pagar sería de: $" +Math.round(interes));
+    function tres_cuotas() {
+        monto = parseInt(prompt("Ingrese el monto a calcular en pesos argentinos: "), 10);
+        if(Number.isNaN(monto)) return alert("Este simulador sólo admite números como parámetro");
+            db_prestamo.push(new Prestamo(0, 3, 0));
+            db_prestamo[0].set_monto(monto);
+            db_prestamo[0].set_interes(15);
+            db_prestamo[0].set_total()
+            db_prestamo[0].get_datos()
 }
 
-function seis_cuotas(prestamo) {
-    prestamo = parseInt(prompt("Ingrese el monto a calcular en pesos argentinos: "), 10);
-    if (Number.isNaN(prestamo)) return alert("Este simulador sólo admite números como parámetro");
-    seis++;
-    for (i = 0; i == 0; i++) {
-        interes = prestamo * 0.25;
-    }
-    alert("Para la cantidad de: $" +prestamo.toFixed(2)+ " , en 6 cuotas, el interés a pagar sería de: $" +Math.round(interes));
+    function seis_cuotas() {
+        monto = parseInt(prompt("Ingrese el monto a calcular en pesos argentinos: "), 10);
+        if(Number.isNaN(prestamo)) return alert("Este simulador sólo admite números como parámetro");
+            db_prestamo.push(new Prestamo(0, 6, 0));
+            db_prestamo[0].set_monto(monto);
+            db_prestamo[0].set_interes(25);
+            db_prestamo[0].set_total()
+	        db_prestamo[0].get_datos()
+}
+    function doce_cuotas() {
+        monto = parseInt(prompt("Ingrese el monto a calcular en pesos argentinos: "), 10);
+        if (Number.isNaN(prestamo)) return alert("Este simulador sólo admite números como parámetro");
+            db_prestamo.push(new Prestamo(0, 12, 0));
+            db_prestamo[0].set_monto(monto);
+            db_prestamo[0].set_interes(35);
+            db_prestamo[0].set_total()
+	        db_prestamo[0].get_datos()
 }
 
-function nueve_cuotas(prestamo) {
-    prestamo = parseInt(prompt("Ingrese el monto a calcular en pesos argentinos: "), 10);
-    if (Number.isNaN(prestamo)) return alert("Este simulador sólo admite números como parámetro");
-    nueve++;
-    for (i = 0; i == 0; i++) {
-        interes = prestamo * 0.35;
-    }
-    alert("Para la cantidad de: $" +prestamo.toFixed(2)+ " , en 9 cuotas, el interés a pagar sería de: $" +Math.round(interes));
-}
-} while (ok == true);
+} while (salir === true)
 
 
 
